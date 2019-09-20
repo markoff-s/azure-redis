d6281c0c76998340df3389afc882b5e771d23ca0
Unknown <markov.slava@gmail.com>
2019-09-19 20:29:21 -0400

    azure redis cache rocks

---
+++ AzureRedisTest/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace AzureRedisTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IDistributedCache _cache;
+
+        public ValuesController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            const string key = "xz3";
+            var values = await _cache.GetStringAsync(key);
+            if (!string.IsNullOrEmpty(values)) return Ok(values);
+
+            // kinda pull from db
+            await Task.Delay(5000);
+            values = JsonConvert.SerializeObject(new[] { "value1", "value2" });
+            _cache.Set(key,
+                Encoding.UTF8.GetBytes(values),
+                new DistributedCacheEntryOptions
+                {
+                    SlidingExpiration = TimeSpan.FromSeconds(10)
+                });
+
+            return Ok(values);
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
